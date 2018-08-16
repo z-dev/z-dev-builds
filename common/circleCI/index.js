71b2f62fa5cb7a427a0ad95e31ad4020c33d2a9c
Alex <altaudiolabs@gmail.com>
2018-08-16 16:06:12 +0100

    Removes unrequired initialisation code for CircleCI

--- common/circleCI/index.js
+++ common/circleCI/index.js
@@ -1,14 +1,7 @@
 import CircleCI from 'circleci'
 
-let circleCI
-let circleCIInitialised = false
-
-if(!circleCIInitialised){
-  circleCIInitialised = true
-  circleCI = new CircleCI({
-    auth: process.env.CIRCLE_TOKEN
-  })
-}
+const circleCI = new CircleCI({
+  auth: process.env.CIRCLE_TOKEN
+})
 
 export default circleCI
-
