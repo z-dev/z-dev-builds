b50b2b46f84872f4b9f3932df8990ce6170f27af
Alex <altaudiolabs@gmail.com>
2018-08-21 13:46:43 +0100

    Updates filtersSelectors name

--- redux/selectors/filters.js
+++ redux/selectors/filters.js
@@ -2,7 +2,7 @@ import { createSelector } from 'reselect-change-memoize'
 import setQueryParameters from 'common/utilities/setQueryParameters'
 
 export default createSelector(
-  'projects',
+  'filters',
   state => state.filters,
   filtersState => {
     setQueryParameters({ showFailed: filtersState.showFailed, projectQuery: filtersState.projectQuery })
