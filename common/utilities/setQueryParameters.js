6039ebf98ca098dae1fd43a9ee71e30f313cbd6f
Alex <altaudiolabs@gmail.com>
2018-08-21 13:51:14 +0100

    Makes setQueryParameters browser check a guard

--- common/utilities/setQueryParameters.js
+++ common/utilities/setQueryParameters.js
@@ -1,12 +1,13 @@
 import Router from 'next/router'
 
 export default queries => {
-  if(process.browser){
-    const filteredQueries = _.pickBy(queries, query => query !== null && query !== undefined && query !== '')
-
-    Router.push({
-      pathname: '/',
-      query: filteredQueries,
-    })
+  if(!process.browser){
+    return
   }
+  const filteredQueries = _.pickBy(queries, query => query !== null && query !== undefined && query !== '')
+
+  Router.push({
+    pathname: '/',
+    query: filteredQueries,
+  })
 }
