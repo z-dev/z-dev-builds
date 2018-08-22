8f9f9094c906323df826286034025811da5f37c9
Alex <altaudiolabs@gmail.com>
2018-08-22 13:34:49 +0100

    Updates lint to include common folder

--- common/circleCI/index.js
+++ common/circleCI/index.js
@@ -1,7 +1,7 @@
 import CircleCI from 'circleci'
 
 const circleCI = new CircleCI({
-  auth: process.env.CIRCLE_TOKEN
+  auth: process.env.CIRCLE_TOKEN,
 })
 
 export default circleCI
--- common/utilities/query.js
+++ common/utilities/query.js
@@ -1,22 +1,23 @@
 import Router from 'next/router'
+import _ from 'lodash'
 import { isBrowser } from './browserChecks'
 
-const formatQuery = query => {
-  return _.chain(query)
+const formatQuery = unformattedQuery => {
+  return _.chain(unformattedQuery)
     .mapValues(query => _.toString(query))
     .pickBy(query => !_.isNil(query) && query !== '')
     .value()
 }
 
 export const setQueryParameters = query => {
-  if(!isBrowser()){
+  if (!isBrowser()) {
     return
   }
 
   const previousQuery = Router.router.query
   const formattedQuery = formatQuery(query)
 
-  if(!_.isEqual(previousQuery, formattedQuery)){
+  if (!_.isEqual(previousQuery, formattedQuery)) {
     Router.push({
       pathname: '/',
       query: formattedQuery,
