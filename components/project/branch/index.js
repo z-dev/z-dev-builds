e7459cac12de6b9b7b48735b7a0f53c59c81f85d
Alex <altaudiolabs@gmail.com>
2018-08-28 09:26:24 +0100

    Makes code DRYer

--- components/project/branch/index.js
+++ components/project/branch/index.js
@@ -3,8 +3,8 @@ import theme from 'styles/theme'
 import BranchRow from './branchRow'
 
 export default props => {
-  const statusColor = props.branch.latestBuild.failed ? theme.colors.red : null
   const branch = props.branch
+  const statusColor = branch.latestBuild.failed ? theme.colors.red : null
   const latestBuild = branch.latestBuild
   const values = [branch.name, latestBuild.time, latestBuild.status]
 
