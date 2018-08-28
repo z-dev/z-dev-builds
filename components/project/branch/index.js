0010038698dadc26477d7f53e3174dc28aebeb53
Alex <altaudiolabs@gmail.com>
2018-08-28 09:31:31 +0100

    Makes code DRYer again

--- components/project/branch/index.js
+++ components/project/branch/index.js
@@ -4,8 +4,8 @@ import BranchRow from './branchRow'
 
 export default props => {
   const branch = props.branch
-  const statusColor = branch.latestBuild.failed ? theme.colors.red : null
   const latestBuild = branch.latestBuild
+  const statusColor = latestBuild.failed ? theme.colors.red : null
   const values = [branch.name, latestBuild.time, latestBuild.status]
 
   return <BranchRow values={values} cell3Color={statusColor} />
